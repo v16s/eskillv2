6a0d103474564fa2facfc51302488b8e69a95c11
Vignesh Shankar <shankar.vignesh@gmail.com>
2019-11-11 20:32:02 +0530

    changed endpoint

--- frontend/util/endpoints.jsx
+++ frontend/util/endpoints.jsx
@@ -1,5 +1,5 @@
 export default {
-  production: 'http://care.srmist.edu.in/eskill/graphql',
+  production: 'https://care.srmist.edu.in/eskill/graphql',
   path: '/eskill',
   dev: 'http://localhost:5000/graphql'
 }
