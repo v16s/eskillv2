3bf4f722c73f1872f6b4a2026c191139676dc2fa
Decimate Labs <decimatelabs@gmail.com>
2020-04-20 02:13:58 +0530

    added reset password

--- frontend/views/Admin/index.jsx
+++ frontend/views/Admin/index.jsx
@@ -1,79 +1,87 @@
-import React from 'react'
-import { Switch, Route } from 'react-router-dom'
-import Dashboard from './Dashboard'
-import DefaultCourses from './DefaultCourses'
-import Questions from './Questions'
-import Progress from './Progress'
+import React from "react";
+import { Switch, Route } from "react-router-dom";
+import Dashboard from "./Dashboard";
+import DefaultCourses from "./DefaultCourses";
+import Questions from "./Questions";
+import Progress from "./Progress";
+import Reset from "../Reset";
 
-import { Tabs, Tab } from '@material-ui/core'
-import { AppBar } from '../../components'
-import { withRouter } from 'react-router-dom'
+import { Tabs, Tab } from "@material-ui/core";
+import { AppBar } from "../../components";
+import { withRouter } from "react-router-dom";
 export default withRouter(
   class Admin extends React.Component {
-    constructor (props) {
-      super(props)
-      let location = 0
+    constructor(props) {
+      super(props);
+      let location = 0;
       switch (props.history.location.pathname) {
-        case '/questions':
-          location = 1
-          break
-        case '/reports':
-          location = 2
-          break
-        case '/defaults':
-          location = 3
-          break
+        case "/questions":
+          location = 1;
+          break;
+        case "/reports":
+          location = 2;
+          break;
+        case "/defaults":
+          location = 3;
+          break;
+        case "/reset":
+          location = 4;
       }
       this.state = {
-        value: location
-      }
+        value: location,
+      };
     }
     handleChange = (e, value) => {
       switch (value) {
         case 0:
-          this.props.history.push('/')
-          break
+          this.props.history.push("/");
+          break;
         case 1:
-          this.props.history.push('/questions')
-          break
+          this.props.history.push("/questions");
+          break;
         case 2:
-          this.props.history.push('/reports')
-          break
+          this.props.history.push("/reports");
+          break;
         case 3:
-          this.props.history.push('/defaults')
-          break
+          this.props.history.push("/defaults");
+          break;
+        case 4:
+          this.props.history.push("/reset");
+          break;
       }
-      this.setState({ value })
-    }
-    render () {
-      const { value } = this.state
+      this.setState({ value });
+    };
+    render() {
+      const { value } = this.state;
       return (
         <div
           style={{
-            width: '100vw',
-            minHeight: '100vh'
+            width: "100vw",
+            minHeight: "100vh",
           }}
         >
           <AppBar />
           <Tabs
             value={value}
-            indicatorColor='primary'
-            textColor='primary'
+            indicatorColor="primary"
+            textColor="primary"
             onChange={this.handleChange}
           >
-            <Tab label='Dashboard' />
-            <Tab label='Questions' />
-            <Tab label='Progress Reports' />
-            <Tab label='Default Courses' />
+            <Tab label="Dashboard" />
+            <Tab label="Questions" />
+            <Tab label="Progress Reports" />
+            <Tab label="Default Courses" />
+            <Tab label="Reset Password" />
           </Tabs>
           <Switch>
-            <Route path='/defaults' component={DefaultCourses} />
-            <Route path='/reports' component={Progress} />
-            <Route path='/questions' component={Questions} />
-            <Route path='/' component={Dashboard} />
+            <Route path="/reset" component={Reset} />
+            <Route path="/defaults" component={DefaultCourses} />
+            <Route path="/reports" component={Progress} />
+            <Route path="/questions" component={Questions} />
+            <Route path="/" component={Dashboard} />
           </Switch>
         </div>
-      )
+      );
     }
   }
-)
+);
