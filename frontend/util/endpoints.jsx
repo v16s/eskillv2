6298e178d22ef96498602afe42a13a381478259a
UndercovercameL <undercover@camel>
2022-02-22 13:19:28 +0530

    final fix

--- frontend/util/endpoints.jsx
+++ frontend/util/endpoints.jsx
@@ -1,5 +1,5 @@
 export default {
   production: process.env.REACT_APP_API_ENDPOINT || 'https://dld.srmist.edu.in/eskill/graphql',
   path: '/eskill',
-  dev: 'http://localhost:5000/graphql'
+  dev: process.env.REACT_APP_API_ENDPOINT || 'https://dld.srmist.edu.in/eskill/graphql',
 }
