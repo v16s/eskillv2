429b827aca0450e9da81f918335be82cacafc8da
Vignesh Shankar <16210686+v16s@users.noreply.github.com>
2022-02-22 11:44:17 +0530

    Update endpoints.jsx

--- frontend/util/endpoints.jsx
+++ frontend/util/endpoints.jsx
@@ -1,5 +1,5 @@
 export default {
-  production: 'https://care.srmist.edu.in/eskill/graphql',
+  production: process.env.REACT_APP_API_ENDPOINT || 'https://dld.srmist.edu.in/eskill/graphql',
   path: '/eskill',
   dev: 'http://localhost:5000/graphql'
 }
